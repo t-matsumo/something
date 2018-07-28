dab99af59962e8d24a2a0463301a450ac778eac9
t-matsumo <tatsukimatsumo@gmail.com>
2018-07-28 22:42:58 +0900

    Update port

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ io.on('connection', function (socket) {
     io.sockets.emit('changeBoard', message);
   });
 });
-
-http.listen(3000, function () {
+http.listen(process.env.PORT || 3000, function () {
   console.log('listening on *:3000');
 });
